7769c9d0ec0daa17822abd2fac4c8927b4ff6700
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-03-12 10:46:51 +0100

    going to go back

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,9 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
       ),
     );
   }
