8e5d0fed74213dc9f49eb86f02a5f021cbc267b6
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-03-28 11:56:17 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Hello, andre!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
