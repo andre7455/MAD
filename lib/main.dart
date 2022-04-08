4a27823784a21e3430c23c6ddf7a10e7b6c37f4f
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-04-08 15:39:41 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,41 @@ class Myapp extends StatefulWidget {
 class _MyappState extends State<Myapp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // widget 1 staat hier
-      home: AppBar(
-        backgroundColor: Color.fromARGB(255, 209, 50, 50),
-        title: Text('KAAS'),
-      )
-    );
+    return SafeArea(
+        child: ListView(
+            children: List.generate(
+                100,
+                ((index) => MaterialApp(
+                    title: 'Flutter Demo',
+                    theme: ThemeData(
+                        primarySwatch: Colors.blue,
+                        scaffoldBackgroundColor: Colors.white10),
+                    debugShowCheckedModeBanner:
+                        false, // Remove the debug banner
+                    home: Scaffold(
+                      body: Row(children: <Widget>[
+                        // Content of my Row
+
+                        //Item 1/4
+                        Container(
+                          padding: const EdgeInsets.all(20),
+                          color: Colors.red,
+                          child: const Text("Item 1"),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(20),
+                          color: Colors.yellow,
+                          child: const Text("Item 2"),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.all(20),
+                            color: Colors.green,
+                            child: const Text("kaas")),
+                        Container(
+                            padding: const EdgeInsets.all(20),
+                            color: Colors.orange,
+                            child: const Text("Uranus"))
+                      ]),
+                    ))))));
   }
 }
