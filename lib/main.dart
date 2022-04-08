d2fec0869bcb4a50915efa2fc748a7edb3310cec
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-04-08 12:57:41 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(Myapp());
 }
 
-class Myapp extends StatelessWidget {
+// no idea what this part is but okey
+class Myapp extends StatefulWidget {
   const Myapp({Key? key}) : super(key: key);
   @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
   Widget build(BuildContext context) {
-    int i = 0;
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 50, 106, 216),
-        title: const Text("green"),
-      ),
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [Text('text1'), Text('kaas')]),
-    ));
+      // widget 1 staat hier
+     home: AppBar(
+       title: Text('KAAS'),
+     ),
+    //  widget 2 komt hopenlijk hier
+    );
   }
 }
