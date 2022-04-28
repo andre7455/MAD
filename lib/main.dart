26a66c1c4a37a17ae7cd0cd350907f6833cd4e8b
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-04-28 22:34:27 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,20 @@
 import 'package:flutter/material.dart';
-
-int i = 0;
-
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({required this.title, Key? key}) : super(key: key);
-  final Widget title;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      child: Row(
-        children: [
-          const IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: null,
-          ),
-          Expanded(
-            child: title,
-          ),
-          const IconButton(
-            icon: Icon(Icons.search),
-            onPressed: null,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context) //
-                  .primaryTextTheme
-                  .headline6,
-            ),
-          ),
-          Row(
-            children: [
-              const Expanded(
-                child: Center(
-                  child: Text('Hello, hell!'),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'My app',
-      home: SafeArea(
-        child: MyScaffold(),
-      ),
-    ),
-  );
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Mobile Application Development'),
+      )),
+    );
+  }
 }
