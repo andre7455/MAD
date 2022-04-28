85e1ff27493ef32670126d207fd64d01ea969a36
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-04-28 15:45:06 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,71 @@
 import 'package:flutter/material.dart';
 
+int i = 0;
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, Key? key}) : super(key: key);
+  final Widget title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: [
+          const IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          const IconButton(
+            icon: Icon(Icons.search),
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: [
+          MyAppBar(
+            title: Text(
+              'Example title',
+              style: Theme.of(context) //
+                  .primaryTextTheme
+                  .headline6,
+            ),
+          ),
+          Row(
+            children: [
+              const Expanded(
+                child: Center(
+                  child: Text('Hello, hell!'),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
 void main() {
   runApp(
-    const Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+    const MaterialApp(
+      title: 'My app',
+      home: SafeArea(
+        child: MyScaffold(),
       ),
     ),
   );
