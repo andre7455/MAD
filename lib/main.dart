f0f26b0579aa7647e5b64161ae1593208adf003b
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-04-28 15:14:29 +0200

    4a27823784a21e3430c23c6ddf7a10e7b6c37f4f.

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(Myapp());
-}
-
-// no idea what this part is but okey
-class Myapp extends StatefulWidget {
-  const Myapp({Key? key}) : super(key: key);
-  @override
-  State<Myapp> createState() => _MyappState();
-}
-
-class _MyappState extends State<Myapp> {
-  @override
-  Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            scaffoldBackgroundColor: Colors.white10),
-        debugShowCheckedModeBanner: false, // Remove the debug banner
-        home: Scaffold(
-            body: Row(children: <Widget>[
-          Column(children: <Widget>[
-            // Content of my Row
-            //Item 1/4
-            Expanded(
-                child: Container(
-              padding: const EdgeInsets.all(20),
-              color: Colors.red,
-              child: const Text("Item 1"),
-            )),
-            Expanded(
-                child: Container(
-              padding: const EdgeInsets.all(20),
-              color: Colors.yellow,
-              child: const Text("Item 2"),
-            )),
-            Expanded(
-                child: Container(
-              padding: const EdgeInsets.all(20),
-              color: Colors.green,
-              child: const Text("Item 2"),
-            )),
-            Expanded(
-                child: Container(
-                    padding: const EdgeInsets.all(20),
-                    color: Colors.orange,
-                    child: const Text("Uranus")))
-          ]),
-<<<<<<< HEAD
-        ]]));
-=======
-    return SafeArea(
-        child: ListView(
-            children: List.generate(
-                100,
-                ((index) => MaterialApp(
-                    title: 'Flutter Demo',
-                    theme: ThemeData(
-                        primarySwatch: Colors.blue,
-                        scaffoldBackgroundColor: Colors.white10),
-                    debugShowCheckedModeBanner:
-                        false, // Remove the debug banner
-                    home: Scaffold(
-                      body: Row(children: <Widget>[
-                        // Content of my Row
-
-                        //Item 1/4
-                        Container(
-                          padding: const EdgeInsets.all(20),
-                          color: Colors.red,
-                          child: const Text("Item 1"),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(20),
-                          color: Colors.yellow,
-                          child: const Text("Item 2"),
-                        ),
-                        Container(
-                            padding: const EdgeInsets.all(20),
-                            color: Colors.green,
-                            child: const Text("kaas")),
-                        Container(
-                            padding: const EdgeInsets.all(20),
-                            color: Colors.orange,
-                            child: const Text("Uranus"))
-                      ]),
-                    ))))));
->>>>>>> parent of 57c9de0 (made a row of bloks)
-=======
-        ])));
->>>>>>> parent of a9b53da (Update main.dart)
-  }
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
 }
